a9333c075956be0a915b97408ae908a55d393457
VJNadar <charismaticjosh84@gmail.com>
2019-01-14 21:13:33 +0530

    minor edit

--- log/src/components/Pages/Notes/Post/Post.js
+++ log/src/components/Pages/Notes/Post/Post.js
@@ -8,8 +8,6 @@ const post=(props)=>{
 
         <Col sm="12" md={{size:"12"}} lg={{size:"8",offset:"2"}}>
 
-            
-
             <Card body onClick={(event)=>props.clicked(event,props.id)} id={props.id}>
 
             <UncontrolledTooltip target={props.id} placement="top" color="primary"><b>Tap to edit</b></UncontrolledTooltip>
